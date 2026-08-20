2365a3991a93003fff6c9f8c447bd644ee40b43f
jesseegedegbe3-collab <jesseegedegbe3@gmail.com>
2026-08-20 15:18:27 -0500

    Set base path for deployment in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ndgill-lawncare/",
   plugins: [react(), vlyPlugin(), tailwindcss()],
   resolve: {
     alias: {
